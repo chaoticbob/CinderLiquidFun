810f64eae16cb0c008eb7adf7cd2dee90f92f1e3
Hai Nguyen <chaoticbob@me.com>
2015-11-23 01:51:46 -0800

    Initial add

---
+++ samples/Testbed/src/Framework/Main.cpp
@@ -0,0 +1,500 @@
+/*
+* Copyright (c) 2006-2007 Erin Catto http://www.box2d.org
+* Copyright (c) 2013 Google, Inc.
+* Copyright (c) 2015 The Cinder Project
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#if ! defined( CINDER_GL_ES )
+	#include "cinder/params/Params.h"
+#endif
+using namespace ci;
+using namespace ci::app;
+
+#include "Render.h"
+#include "Test.h"
+#include "Arrow.h"
+#include "FullscreenUI.h"
+#include "ParticleParameter.h"
+
+#include <stdio.h>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+namespace TestMain
+{
+
+namespace {
+	int32 testIndex = 0;
+	int32 testSelection = 0;
+	int32 testCount = 0;
+	TestEntry* entry;
+	Test* test;
+	Settings settings;
+	int32 width = 640;
+	int32 height = 540;
+	float settingsHz = 60.0;
+
+	float32 viewZoom = 1.0f;
+	int tx, ty, tw, th;
+	bool rMouseDown = false;
+
+	// State of the mouse on the previous call of Mouse().
+	int32 previousMouseState = -1;
+	b2Vec2 lastp;
+	b2Vec2 extents;
+
+	// Fullscreen UI object.
+	FullscreenUI fullscreenUI;
+	// Used to control the behavior of particle tests.
+	ParticleParameter particleParameter;
+}
+
+// Set whether to restart the test on particle parameter changes.
+// This parameter is re-enabled when the test changes.
+void SetRestartOnParticleParameterChange(bool enable)
+{
+	particleParameter.SetRestartOnChange(enable);
+}
+
+// Set the currently selected particle parameter value.  This value must
+// match one of the values in TestMain::k_particleTypes or one of the values
+// referenced by particleParameterDef passed to SetParticleParameters().
+uint32 SetParticleParameterValue(uint32 value)
+{
+	const int32 index = particleParameter.FindIndexByValue(value);
+	// If the particle type isn't found, so fallback to the first entry in the
+	// parameter.
+	particleParameter.Set(index >= 0 ? index : 0);
+	return particleParameter.GetValue();
+}
+
+// Get the currently selected particle parameter value and enable particle
+// parameter selection arrows on Android.
+uint32 GetParticleParameterValue()
+{
+	// Enable display of particle type selection arrows.
+	fullscreenUI.SetParticleParameterSelectionEnabled(true);
+	return particleParameter.GetValue();
+}
+
+// Override the default particle parameters for the test.
+void SetParticleParameters(
+	const ParticleParameter::Definition * const particleParameterDef,
+	const uint32 particleParameterDefCount)
+{
+	particleParameter.SetDefinition(particleParameterDef,
+									particleParameterDefCount);
+}
+
+} // namespace TestMain
+
+using namespace TestMain;
+
+static b2Vec2 ConvertScreenToWorld(int32 x, int32 y)
+{
+	float32 u = x / float32(tw);
+	float32 v = (th - y) / float32(th);
+
+	b2Vec2 lower = settings.viewCenter - extents;
+	b2Vec2 upper = settings.viewCenter + extents;
+
+	b2Vec2 p;
+	p.x = (1.0f - u) * lower.x + u * upper.x;
+	p.y = (1.0f - v) * lower.y + v * upper.y;
+	return p;
+}
+
+//! \class TestbedApp
+//!
+//!
+class TestbedApp : public App {
+public:
+
+	virtual void setup() override;
+	virtual void mouseDown( MouseEvent event ) override;
+	virtual void mouseUp( MouseEvent event ) override;
+	virtual void mouseDrag( MouseEvent event ) override;
+	virtual void keyDown( KeyEvent event ) override;
+	virtual void keyUp( KeyEvent event ) override;
+	virtual void resize() override;
+	virtual void update() override;
+	virtual void draw() override;
+
+private:
+	void resizeView( int width, int height );
+
+	params::InterfaceGl		mParams;
+};
+
+void TestbedApp::setup()
+{
+	std::vector<std::string> testNames;
+
+	testCount = 0;
+	while( nullptr != g_testEntries[testCount].createFcn ) {
+		testNames.push_back( g_testEntries[testCount].name );
+		++testCount;
+	}
+
+	testIndex = b2Clamp(testIndex, 0, testCount-1);
+	testSelection = testIndex;
+
+	entry = g_testEntries + testIndex;
+	if (entry && entry->createFcn) {
+		test = entry->createFcn();
+		testSelection = testIndex;
+		testIndex = -1;
+	}
+
+#if ! defined( CINDER_GL_ES )
+	mParams = params::InterfaceGl( "Params", ivec2( 250, 400 ) );
+	mParams.addParam( "Tests", testNames, &testSelection );
+	mParams.addSeparator();
+	mParams.addParam( "Vel Iters", &settings.velocityIterations, "min=1 max=500 step=1" );
+	mParams.addParam( "Pos Iters", &settings.positionIterations, "min=0 max=100 step=1" );
+	mParams.addParam( "Pcl Iters", &settings.particleIterations, "min=1 max=100 step=1" );
+	mParams.addParam( "Hertz", &settingsHz, "min=1 max=100 step=1" );
+	mParams.addSeparator();
+	mParams.addParam( "Sleep", &settings.enableSleep );
+	mParams.addParam( "Warm Starting", &settings.enableWarmStarting );
+	mParams.addParam( "Time of Impact", &settings.enableContinuous );
+	mParams.addParam( "Sub-Stepping", &settings.enableSubStepping );
+	mParams.addParam( "Strict Particle/Body Contacts", &settings.strictContacts );
+	mParams.addSeparator( "Draw" );
+	mParams.addParam( "Shapes", &settings.drawShapes );
+	mParams.addParam( "Particles", &settings.drawParticles );
+	mParams.addParam( "Joints", &settings.drawJoints );
+	mParams.addParam( "AABBs", &settings.drawAABBs) ;
+	mParams.addParam( "Contact Points", &settings.drawContactPoints );
+	mParams.addParam( "Contact Normals", &settings.drawContactNormals );
+	mParams.addParam( "Contact Impulses", &settings.drawContactImpulse );
+	mParams.addParam( "Friction Impulses", &settings.drawFrictionImpulse );
+	mParams.addParam( "Center of Masses", &settings.drawCOMs );
+	mParams.addParam( "Statistics", &settings.drawStats );
+	mParams.addParam( "Profile", &settings.drawProfile );
+#endif
+}
+
+void TestbedApp::mouseDown( MouseEvent event )
+{
+	int x = event.getPos().x;
+	int y = event.getPos().y;
+	int button = GLUT_NONE;
+	int state = GLUT_NONE;
+	int mod = GLUT_NONE;
+	if( event.isLeft() ) {
+		button = GLUT_LEFT_BUTTON;
+		if( event.isLeftDown() ) {
+			state = GLUT_DOWN;
+		}
+	}
+	else if( event.isRight() ) {
+		button = GLUT_RIGHT_BUTTON;
+		if( event.isRightDown() ) {
+			state = GLUT_DOWN;
+		}
+	}
+
+	if (button == GLUT_LEFT_BUTTON)
+	{
+		b2Vec2 p = ConvertScreenToWorld(x, y);
+
+		switch (fullscreenUI.Mouse(button, state, previousMouseState, p))
+		{
+		case FullscreenUI::e_SelectionTestPrevious:
+			testSelection = std::max(0, testSelection - 1);
+			break;
+		case FullscreenUI::e_SelectionTestNext:
+			if (g_testEntries[testSelection + 1].name) testSelection++;
+			break;
+		case FullscreenUI::e_SelectionParameterPrevious:
+			particleParameter.Decrement();
+			break;
+		case FullscreenUI::e_SelectionParameterNext:
+			particleParameter.Increment();
+			break;
+		default:
+			break;
+		}
+
+		if (state == GLUT_DOWN)
+		{
+			b2Vec2 p = ConvertScreenToWorld(x, y);
+			if (mod == GLUT_ACTIVE_SHIFT)
+			{
+				test->ShiftMouseDown(p);
+			}
+			else
+			{
+				test->MouseDown(p);
+			}
+		}
+
+		/*
+		if (state == GLUT_UP)
+		{
+			test->MouseUp(p);
+		}
+		*/
+	}
+	else if (button == GLUT_RIGHT_BUTTON)
+	{
+		if (state == GLUT_DOWN)
+		{
+			lastp = ConvertScreenToWorld(x, y);
+			rMouseDown = true;
+		}
+
+		/*
+		if (state == GLUT_UP)
+		{
+			rMouseDown = false;
+		}
+		*/
+	}
+	previousMouseState = state;
+}
+
+void TestbedApp::mouseUp( MouseEvent event )
+{
+	int x = event.getPos().x;
+	int y = event.getPos().y;
+
+	int state = GLUT_NONE;
+	if( event.isLeft() ) {
+		state = GLUT_UP;
+		b2Vec2 p = ConvertScreenToWorld(x, y);
+		test->MouseUp(p);
+	}
+	else if( event.isRight() ) {
+		state = GLUT_UP;
+		rMouseDown = false;
+	}
+	previousMouseState = state;
+}
+
+void TestbedApp::mouseDrag( MouseEvent event )
+{
+	int x = event.getPos().x;
+	int y = event.getPos().y;
+	b2Vec2 p = ConvertScreenToWorld(x, y);
+
+	if (fullscreenUI.GetSelection() == FullscreenUI::e_SelectionNone)
+	{
+		test->MouseMove(p);
+	}
+
+	if (rMouseDown)
+	{
+		b2Vec2 diff = p - lastp;
+		settings.viewCenter.x -= diff.x;
+		settings.viewCenter.y -= diff.y;
+		resizeView( width, height );
+		lastp = ConvertScreenToWorld(x, y);
+	}
+}
+
+void TestbedApp::keyDown(KeyEvent event)
+{
+	char key = event.getChar();
+
+console() << "key: " << key << " : " << (int)key << std::endl;
+
+	switch (key)
+	{
+	case 27:
+		quit();
+		break;
+
+		// Press 'z' to zoom out.
+	case 'z':
+	case 'Z':
+		viewZoom = b2Min(1.1f * viewZoom, 20.0f);
+		resizeView( width, height );
+		break;
+
+		// Press 'x' to zoom in.
+	case 'x':
+	case 'X':
+		viewZoom = b2Max(0.9f * viewZoom, 0.02f);
+		resizeView( width, height );
+		break;
+
+		// Press 'r' to reset.
+	case 'r':
+	case 'R':
+		delete test;
+		test = entry->createFcn();
+		break;
+
+		// Press space to launch a bomb.
+	case ' ':
+		if (test)
+		{
+			test->LaunchBomb();
+		}
+		break;
+
+	case 'p':
+	case 'P':
+		settings.pause = !settings.pause;
+		break;
+
+		// Press [ to prev test.
+	case '[':
+		--testSelection;
+		if (testSelection < 0)
+		{
+			testSelection = testCount - 1;
+		}
+		break;
+
+		// Press ] to next test.
+	case ']':
+		++testSelection;
+		if (testSelection == testCount)
+		{
+			testSelection = 0;
+		}
+		break;
+
+		// Press < to select the previous particle parameter setting.
+	case '<':
+		particleParameter.Decrement();
+		break;
+
+		// Press > to select the next particle parameter setting.
+	case '>':
+		particleParameter.Increment();
+		break;
+
+	default:
+		if( test ) {
+			test->Keyboard(key);
+		}
+	}
+}
+
+void TestbedApp::keyUp( KeyEvent event )
+{
+	char key = event.getChar();
+	if (test) {
+		test->KeyboardUp( key );
+	}
+}
+
+void TestbedApp::resizeView( int viewWidth, int viewHeight )
+{
+	width  = viewWidth;
+	height = viewHeight;
+
+	tx = 0;
+	ty = 0;
+	tw = width;
+	th = height;
+
+	gl::viewport( tx, ty, tw, th );
+
+	float32 ratio = th ? float32(tw) / float32(th) : 1;
+
+	extents = ratio >= 1 ? b2Vec2(ratio * 25.0f, 25.0f) : b2Vec2(25.0f, 25.0f / ratio);
+	extents *= viewZoom;
+
+	b2Vec2 lower = settings.viewCenter - extents;
+	b2Vec2 upper = settings.viewCenter + extents;
+
+	// L/R/B/T
+	LoadOrtho2DMatrix( getWindowWidth(), getWindowHeight(), lower.x, upper.x, lower.y, upper.y);
+
+	if (fullscreenUI.GetEnabled())
+	{
+		fullscreenUI.SetViewParameters(&settings.viewCenter, &extents);
+	}
+}
+
+void TestbedApp::resize()
+{
+	resizeView( getWindowWidth(), getWindowHeight() );
+}
+
+void TestbedApp::update()
+{
+
+}
+
+void TestbedApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) );
+
+	settings.hz = settingsHz;
+
+	test->Step(&settings);
+
+	// Update the state of the particle parameter.
+	bool restartTest;
+	const bool changed = particleParameter.Changed(&restartTest);
+	B2_NOT_USED(changed);
+
+	test->DrawTitle(entry->name);
+
+#if ! defined( CINDER_GL_ES )
+	mParams.draw();
+#endif
+
+	if (testSelection != testIndex || restartTest)
+	{
+		fullscreenUI.Reset();
+		if (!restartTest) particleParameter.Reset();
+
+		testIndex = testSelection;
+		delete test;
+		entry = g_testEntries + testIndex;
+		test = entry->createFcn();
+		viewZoom = test->GetDefaultViewZoom();
+		settings.viewCenter.Set(0.0f, 20.0f * viewZoom);
+		resizeView( width, height );
+	}
+
+	// print world step time stats every 600 frames
+	static int s_printCount = 0;
+	static b2Stat st;
+	st.Record(settings.stepTimeOut);
+
+	const int STAT_PRINT_INTERVAL = 600;
+	if( settings.printStepTimeStats && st.GetCount() == STAT_PRINT_INTERVAL ) {
+
+		console() << "World Step Time samples "
+		          << s_printCount*STAT_PRINT_INTERVAL << "-" << (s_printCount+1)*STAT_PRINT_INTERVAL-1 << ": "
+		          << st.GetMin() << "min " <<  st.GetMax() << "max " <<  st.GetMean() << "avg (ms)" << std::endl;
+		s_printCount++;
+	}
+}
+
+void prepareSettings( TestbedApp::Settings *settings )
+{
+	std::stringstream title;
+	title << "Cinder LiquidFun" << " | " << "Box2D Version " << b2_version.major << "." << b2_version.minor << "." << b2_version.revision;
+	settings->setTitle( title.str() );
+	settings->setWindowSize( 1280, 800 );
+}
+
+CINDER_APP( TestbedApp, RendererGl, prepareSettings )
---
+++ samples/Testbed/src/Framework/Render.cpp
@@ -0,0 +1,315 @@
+/*
+* Copyright (c) 2006-2007 Erin Catto http://www.box2d.org
+* Copyright (c) 2013 Google, Inc.
+* Copyright (c) 2015 The Cinder Project
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "Render.h"
+//
+#include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/gl/TextureFont.h"
+#include "cinder/Surface.h"
+#include "cinder/TriMesh.h"
+using namespace ci;
+
+#include <stdio.h>
+#include <stdarg.h>
+
+// Global vars for drawing in Cinder
+static int gWindowWidth = 0;
+static int gWindowHeight = 0;
+static CameraOrtho gOrthoCam;
+static gl::TextureFontRef gFont18;
+static gl::TextureRef gParticleTex;
+
+const float kPointSize = 0.05f/40.0f;
+static float gPointScale = kPointSize;
+
+void LoadOrtho2DMatrix( int windowWidth, int windowHeight, double left, double right, double bottom, double top)
+{
+	std::cout << left << ", " << right << ", " << bottom << ", " << top << std::endl;
+
+	gWindowWidth = windowWidth;
+	gWindowHeight = windowHeight;
+
+	gOrthoCam.setOrtho( left, right, bottom, top, -1.0f, 1.0f );
+
+	gPointScale = kPointSize*(top - bottom);
+}
+
+void DebugDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
+{
+	gl::setMatrices( gOrthoCam );
+
+	gl::color( color.r, color.g, color.b );
+	gl::begin( GL_LINE_LOOP );
+	for( int32 i = 0; i < vertexCount; ++i ) {
+		gl::vertex( vec2( vertices[i].x, vertices[i].y ) );
+	}
+	gl::end();
+}
+
+void DebugDraw::DrawFlatPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
+{
+	gl::setMatrices( gOrthoCam );
+
+	gl::color( color.r, color.g, color.b, 1.0f );
+	gl::begin( GL_TRIANGLE_FAN );
+	for( int32 i = 0; i < vertexCount; ++i ) {
+		gl::vertex( vec2( vertices[i].x, vertices[i].y ) );
+	}
+	gl::end();
+}
+
+void DebugDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
+{
+	gl::setMatrices( gOrthoCam );
+
+	{
+		gl::ScopedBlendAlpha scopedBlend();
+
+		gl::color( 0.5f * color.r, 0.5f * color.g, 0.5f * color.b, 0.5f );
+		gl::begin( GL_TRIANGLE_FAN );
+		for( int32 i = 0; i < vertexCount; ++i ) {
+			gl::vertex( vec2( vertices[i].x, vertices[i].y ) );
+		}
+		gl::end();
+	}
+
+	gl::color( color.r, color.g, color.b, 1.0f );
+	gl::begin( GL_LINE_LOOP );
+	for( int32 i = 0; i < vertexCount; ++i ) {
+		gl::vertex( vec2( vertices[i].x, vertices[i].y ) );
+	}
+	gl::end();
+}
+
+void DebugDraw::DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color)
+{
+	gl::setMatrices( gOrthoCam );
+
+	gl::color( color.r, color.g, color.b );
+	gl::drawStrokedCircle( vec2( center.x, center.y ), radius, 16 );
+}
+
+float smoothstep(float x) { return x * x * (3 - 2 * x); }
+
+void DebugDraw::DrawParticles(const b2Vec2 *centers, float32 radius, const b2ParticleColor *colors, int32 count)
+{
+	if( ! gParticleTex ) {
+		const int TSIZE = 64;
+		unsigned char tex[TSIZE][TSIZE][4];
+		for (int y = 0; y < TSIZE; y++)
+		{
+			for (int x = 0; x < TSIZE; x++)
+			{
+				float fx = (x + 0.5f) / TSIZE * 2 - 1;
+				float fy = (y + 0.5f) / TSIZE * 2 - 1;
+				float dist = sqrtf(fx * fx + fy * fy);
+				unsigned char intensity = (unsigned char)(dist <= 1 ? smoothstep(1 - dist) * 255 : 0);
+				tex[y][x][0] = tex[y][x][1] = tex[y][x][2] = 128;
+				tex[y][x][3] = intensity;
+			}
+		}
+
+		SurfaceRef surf = Surface::create( (uint8_t*)tex, TSIZE, TSIZE, TSIZE*4, SurfaceChannelOrder::RGBA );
+		gParticleTex = gl::Texture::create( *surf );
+	}
+
+	const float particle_size_multiplier = 2.0f;  // because of falloff
+	const float pointSize = radius  * particle_size_multiplier;
+
+	// Not the fastest way to draw particles
+	TriMesh mesh( TriMesh::Format().positions( 2 ).colors( 4 ).texCoords() );
+	for( int32 i = 0; i < count; ++i ) {
+		// Counter-clockwise
+		vec2 P0 = vec2( centers[i].x, centers[i].y ) + vec2( -pointSize, -pointSize );
+		vec2 P1 = vec2( centers[i].x, centers[i].y ) + vec2(  pointSize, -pointSize );
+		vec2 P2 = vec2( centers[i].x, centers[i].y ) + vec2(  pointSize,  pointSize );
+		vec2 P3 = vec2( centers[i].x, centers[i].y ) + vec2( -pointSize,  pointSize );
+		vec2 uv0 = vec2( 0, 0 );
+		vec2 uv1 = vec2( 1, 0 );
+		vec2 uv2 = vec2( 1, 1 );
+		vec2 uv3 = vec2( 0, 1 );
+		ColorA c = ColorA( 1.0f, 1.0f, 1.0f, 1.0f );
+		if( nullptr != colors ) {
+			c = ColorA( colors[i].r/255.0f, colors[i].g/255.0f, colors[i].b/255.0f, colors[i].a/255.0f );
+		}
+		mesh.appendPosition( P0 );
+		mesh.appendPosition( P1 );
+		mesh.appendPosition( P2 );
+		mesh.appendPosition( P3 );
+		mesh.appendTexCoord0( uv0 );
+		mesh.appendTexCoord0( uv1 );
+		mesh.appendTexCoord0( uv2 );
+		mesh.appendTexCoord0( uv3 );
+		mesh.appendColorRgba( c );
+		mesh.appendColorRgba( c );
+		mesh.appendColorRgba( c );
+		mesh.appendColorRgba( c );
+		size_t n = mesh.getNumVertices();
+		uint32_t v0 = n - 4;
+		uint32_t v1 = n - 3;
+		uint32_t v2 = n - 2;
+		uint32_t v3 = n - 1;
+		mesh.appendTriangle( v0, v1, v2 );
+		mesh.appendTriangle( v0, v2, v3 );
+	}
+
+	gl::setMatrices( gOrthoCam );
+
+	gl::ScopedBlendAlpha scopedBlend();
+
+	gl::GlslProgRef shader = gl::context()->getStockShader( gl::ShaderDef().texture( gParticleTex ).color() );
+	gl::ScopedGlslProg scopedShader( shader );
+
+	gl::ScopedTextureBind scopedTex( gParticleTex, 0 );
+
+	gl::draw( mesh );
+}
+
+void DebugDraw::DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color)
+{
+	gl::setMatrices( gOrthoCam );
+
+	gl::ScopedBlendAlpha scopedBlend();
+
+	gl::color( 0.5f * color.r, 0.5f * color.g, 0.5f * color.b, 0.5f );
+	gl::drawSolidCircle( vec2( center.x, center.y ), radius, 16 );
+
+	gl::color( color.r, color.g, color.b, 1.0f );
+	gl::drawStrokedCircle( vec2( center.x, center.y ), radius, 16 );
+
+	b2Vec2 p = center + radius * axis;
+	gl::drawLine( vec2( center.x, center.y ), vec2( p.x, p.y ) );
+}
+
+void DebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color)
+{
+	gl::setMatrices( gOrthoCam );
+
+	gl::color( color.r, color.g, color.b );
+	gl::drawLine( vec2( p1.x, p1.y ), vec2( p2.x, p2.y ) );
+}
+
+void DebugDraw::DrawTransform(const b2Transform& xf)
+{
+	gl::setMatrices( gOrthoCam );
+
+	b2Vec2 p1 = xf.p, p2;
+	const float32 k_axisScale = 0.4f;
+
+	gl::color( 1.0f, 0.0f, 0.0f );
+	p2 = p1 + k_axisScale * xf.q.GetXAxis();
+	gl::drawLine( vec2( p1.x, p1.y ), vec2( p2.x, p2.y ) );
+
+	gl::color( 0.0f, 1.0f, 0.0f );
+	p2 = p1 + k_axisScale * xf.q.GetYAxis();
+	gl::drawLine( vec2( p1.x, p1.y ), vec2( p2.x, p2.y ) );
+}
+
+void DebugDraw::DrawPoint(const b2Vec2& p, float32 size, const b2Color& color)
+{
+	gl::setMatrices( gOrthoCam );
+
+	gl::color( color.r, color.g, color.b );
+
+	size *= gPointScale;
+	Rectf r = Rectf( p.x - size, p.y - size, p.x + size, p.y + size );
+	gl::drawSolidRect( r );
+}
+
+void DebugDraw::DrawString(int x, int y, const char *string, ...)
+{
+	if( ! gFont18 ) {
+		gFont18 = gl::TextureFont::create( ci::Font( "Arial", 18.0f ) );
+	}
+
+	char buffer[128];
+
+	va_list arg;
+	va_start(arg, string);
+	vsprintf(buffer, string, arg);
+	va_end(arg);
+
+	std::string s = std::string( buffer, strlen( buffer ) );
+
+	// Use a pixel aligned camera with origin at top left
+	gl::setMatricesWindow( gWindowWidth, gWindowHeight, true );
+
+	gl::color( 0.9f, 0.6f, 0.6f );
+	// Cinder draws using baseline so offset on y-axis
+	gFont18->drawString( s, vec2( x, y + 18.0f ) );
+}
+
+void DebugDraw::DrawString(const b2Vec2& p, const char *string, ...)
+{
+/*
+#if !defined(__ANDROID__) && !defined(__IOS__)
+	char buffer[128];
+
+	va_list arg;
+	va_start(arg, string);
+	vsprintf(buffer, string, arg);
+	va_end(arg);
+
+	glColor3f(0.5f, 0.9f, 0.5f);
+	glRasterPos2f(p.x, p.y);
+
+	int32 length = (int32)strlen(buffer);
+	for (int32 i = 0; i < length; ++i)
+	{
+		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, buffer[i]);
+	}
+
+	glPopMatrix();
+#endif // !defined(__ANDROID__) && !defined(__IOS__)
+*/
+}
+
+void DebugDraw::DrawAABB(b2AABB* aabb, const b2Color& c)
+{
+	gl::color( c.r, c.g, c.b );
+
+	Rectf r = Rectf( aabb->lowerBound.x, aabb->lowerBound.y, aabb->upperBound.x, aabb->upperBound.y );
+	gl::drawStrokedRect( r );
+}
+
+float ComputeFPS()
+{
+/*
+	static bool debugPrintFrameTime = false;
+	static int lastms = 0;
+	int curms = glutGet(GLUT_ELAPSED_TIME);
+	int delta = curms - lastms;
+	lastms = curms;
+
+	static float dsmooth = 16;
+	dsmooth = (dsmooth * 30 + delta) / 31;
+
+	if ( debugPrintFrameTime )
+	{
+#ifdef ANDROID
+		__android_log_print(ANDROID_LOG_VERBOSE, "Testbed", "msec = %f", dsmooth);
+#endif
+	}
+
+	return dsmooth;
+*/
+	return 0.0f;
+}
